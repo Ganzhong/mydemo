f61f065ce727089f2db50306116df1e439c4a57a
ganzhong <446386449@qq.com>
2019-08-23 19:38:12 +0800

    gan第一次

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: In User Settings Editl
+ * @Author: your name
+ * @Date: 2019-08-23 19:37:13
+ * @LastEditTime: 2019-08-23 19:37:32
+ * @LastEditors: Please set LastEditors
+ */
+window.onload = function () { 
+    
+}
